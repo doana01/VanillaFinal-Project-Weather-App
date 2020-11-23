ba80c92e658d2a3e2583bba00f02228a72570e9e
doana <artlovevintage@gmail.com>
2020-11-23 13:09:47 +0100

    corrected js files
    .date, hours and minutes and capitalize the first letter

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 function formatDate(timestamp) {
-    let date = newDate(timestamp);
-    let hours = date.gethours();
+    let date = new Date(timestamp);
+    let hours = date.getHours();
     if (hours < 10) {
         hours = `0${hours}`;
     }
-    let minutes = date.getminutes();
+    let minutes = date.getMinutes();
     if (minutes < 10) {
         minutes = `0${minutes}`;
     }
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],;
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let day = days[date.getDay()];
 
-    return `${day},  ${hours} :  ${minutes}`;
+    return `${day}, ${hours}:${minutes}`;
 }
 function displayTemperature(response) { 
     let temperatureElement = document.querySelector("#temperature");
@@ -26,7 +26,7 @@ function displayTemperature(response) {
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
-      dateElement.innerHTML =formatDate(response.data.dt * 1000);
+      dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "a443edec30a183c88b00d1c6adfc3dcd";
