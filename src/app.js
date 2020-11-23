0d062c748428929ddb799fe30a702abe236d0045
doana <artlovevintage@gmail.com>
2020-11-23 12:14:12 +0100

    started adding codes to js file
    . api key
    .api url
    . function date format
    .function display temperature

---
+++ src/app.js
@@ -0,0 +1,31 @@
+function formatDate(timestamp) {
+    let date = newDate(timestamp);
+    let hours = date.gethours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getminutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+
+    return `${day},  ${hours} :  ${minutes}`;
+}
+function displayTemperature(response) { 
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
+    
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+}
+
+let apiKey = "a443edec30a183c88b00d1c6adfc3dcd";
+let city = "San Francisco";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
